8048e3b57691c5da42e7fa14571dcf570de98aa0
rrrrrr111 <curdes@gmail.com>
2019-06-13 15:13:29 +0300

    Project frame

---
+++ server-web/src/main/java/ru/roman/app/StartApp.java
@@ -0,0 +1,19 @@
+package ru.roman.app;
+
+
+/**
+ * Application starting point
+ */
+public class StartApp {
+
+
+    public static void main(String[] args) {
+
+        logBaseUrl();
+    }
+
+    private static void logBaseUrl() {
+
+        System.out.println("App started at http://127.0.0.1:8080");
+    }
+}
